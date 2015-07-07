7b3f687a7326d3219e1b49ee7d62259ca5660fa6
Dref360 <frederic.branchaud.charron@gmail.com>
2015-07-06 22:42:09 -0400

    Commence, le pkt DNS fuck a cause de l'url je crois

---
+++ IFT585-TP3/DNSPacket.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFT585_TP3
+{
+    /*ID - ushort
+      Flag - ushort
+     *Number of Questions - ushort (1)
+     *Number of Answer - ushort (0)
+     *Number of authority - ushort (0)
+     *Number of additionnal RR - ushort (0)
+     *Questions ->NbLabel - byte (1) 
+     *            Question*
+     *Question -> url (ASCII)
+     *            type - ushort (1)
+     *            class - ushort (1)
+     *            TTL - 32bit (0) Dont cache
+     */
+    class DNSPacket
+    {
+        public byte[] CreatePacket(string url)
+        {
+            Random random = new Random();
+            IEnumerable<byte> id = BitConverter.GetBytes((ushort)random.Next());
+            IEnumerable<byte> flag = BitConverter.GetBytes((ushort)0x0100).Reverse();
+            IEnumerable<byte> nbQuest = BitConverter.GetBytes((ushort)1).Reverse();
+            IEnumerable<byte> nbAns = BitConverter.GetBytes((ushort)0), nbAuth = BitConverter.GetBytes((ushort)0), nbAdd = BitConverter.GetBytes((ushort)0);
+            IEnumerable<byte> nbLabel = new byte[] { 3 };
+            IEnumerable<byte> nameBytes = System.Text.Encoding.UTF7.GetBytes(url);
+            IEnumerable<byte> type = BitConverter.GetBytes((ushort)1).Reverse(), pktClass = BitConverter.GetBytes((ushort)1).Reverse();
+            //byte[] ttl = BitConverter.GetBytes((ushort)0);
+
+            return id.Concat(flag).Concat(nbQuest).Concat(nbAns).Concat(nbAuth).Concat(nbAdd).Concat(nbLabel).Concat(nameBytes).Concat(new byte[]{0}).Concat(type).Concat(pktClass).ToArray();
+        }
+    }
+}
--- IFT585-TP3/Program.cs
+++ IFT585-TP3/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,14 @@ namespace IFT585_TP3
     {
         static void Main(string[] args)
         {
+            DNSPacket packet = new DNSPacket();
+            UdpClient client = new UdpClient();
+            client.Connect(IPAddress.Parse("8.8.8.8"),53);
+            var pkt = packet.CreatePacket("www.google.com");
+            int size = client.Send(pkt, pkt.Length);
+            IPEndPoint end = new IPEndPoint(IPAddress.Any,0);
+            var data = client.Receive(ref end);
+            Console.Read();
         }
     }
 }
